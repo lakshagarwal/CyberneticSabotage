b76002c38f35979381a42c80633634c48cbbab0d
Laksh Agarwal <laksh212@gmail.com>
2023-06-07 11:49:18 -0700

    Fix styling in main.js

--- main.js
+++ main.js
@@ -2,20 +2,18 @@ const textarea = document.querySelector("#query-textarea");
 const displayText = document.querySelector(".display-text");
 const form = document.querySelector("#query-form");
 
-let queryHistory = [];
+const queryHistory = [];
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  let query = textarea.value;
+  const query = textarea.value;
   queryHistory.push(query);
 
   displayText.innerHTML = "";
   queryHistory.forEach((query, index) => {
-    displayText.innerHTML += `<p>Query ${index + 1}: ${query.replace(
-      /\n/g,
-      "<br>"
-    )}</p>`;
+    displayText.innerHTML +=
+      "<p>Query " + (index + 1) + ": " + query.replace(/\n/g, "<br>") + "</p>";
   });
 
   textarea.value = "";
