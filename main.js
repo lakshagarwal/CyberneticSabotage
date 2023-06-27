0a3880e1d31c9475d3ad1d4cf614e5cb482adde4
Laksh Agarwal <laksh212@gmail.com>
2023-06-27 13:01:11 -0700

    Add code to run SQL query

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/* global initSqlJs */
+
 const textarea = document.querySelector('#query-textarea')
 const displayText = document.querySelector('.display-text')
 const form = document.querySelector('#query-form')
@@ -50,7 +52,67 @@ form.addEventListener('submit', function (event) {
 
   textarea.value = ''
 
-  // Send the query to the SQL parser here
+  executeQuery(query, queryHistory.length - 1)
 })
+
+function executeQuery (query, index) {
+  initSqlJs().then(function (SQL) {
+    fetch('database/main.db')
+      .then(response => response.arrayBuffer())
+      .then(buffer => {
+        const db = new SQL.Database(new Uint8Array(buffer))
+        const stmt = db.prepare(query)
+
+        const table = document.createElement('table')
+        table.style.borderCollapse = 'collapse'
+        table.style.width = '100%'
+
+        const thead = document.createElement('thead')
+        const tbody = document.createElement('tbody')
+
+        let headers = null
+        while (stmt.step()) {
+          const row = stmt.getAsObject()
+
+          if (!headers) {
+            headers = Object.keys(row)
+            const headerRow = document.createElement('tr')
+            headers.forEach(header => {
+              const th = document.createElement('th')
+              th.textContent = header
+              th.style.border = '1px solid '
+              th.style.padding = '8px'
+              headerRow.appendChild(th)
+            })
+            thead.appendChild(headerRow)
+          }
+
+          const dataRow = document.createElement('tr')
+          headers.forEach(header => {
+            const td = document.createElement('td')
+            td.textContent = row[header]
+            td.style.border = '1px solid'
+            td.style.padding = '8px'
+            dataRow.appendChild(td)
+          })
+          tbody.appendChild(dataRow)
+        }
+
+        table.appendChild(thead)
+        table.appendChild(tbody)
+
+        const resultsElement = document.createElement('div')
+        resultsElement.appendChild(table)
+
+        const queryElement = displayText.querySelectorAll('p')[index]
+        queryElement.insertAdjacentElement('afterend', resultsElement)
+
+        db.close()
+      })
+      .catch(error => {
+        console.error('Error fetching the SQLite file:', error)
+      })
+  })
+}
 
 startGame()
