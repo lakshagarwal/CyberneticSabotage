9f34c16d65a60c49786483d222ef24be8b39fc04
Laksh Agarwal <laksh212@gmail.com>
2023-06-28 22:16:21 -0700

    Add error message functionality

--- main.js
+++ main.js
@@ -40,80 +40,84 @@ form.addEventListener('submit', function (event) {
 
   const query = textarea.value
   queryHistory.push(query)
-
   score += 5
   updateScore()
 
-  const index = queryHistory.length - 1
-  displayText.innerHTML +=
-    '<p>Query ' + (index + 1) + ': ' + query.replace(/\n/g, '<br>') + '</p>'
+  const queryWrapper = document.createElement('div')
+
+  const queryParagraph = document.createElement('p')
+  queryParagraph.textContent = 'Query ' + (queryHistory.length) + ': ' + query
+  queryWrapper.appendChild(queryParagraph)
 
   textarea.value = ''
-
   scrollToBottom()
-  executeQuery(query, index)
+
+  executeQuery(query, queryHistory.length - 1, queryWrapper)
+
+  displayText.appendChild(queryWrapper)
+  scrollToBottom()
 })
 
-function executeQuery (query, index) {
+function executeQuery (query, index, queryWrapper) {
   initSqlJs().then(function (SQL) {
     fetch('database/main.db')
       .then(response => response.arrayBuffer())
       .then(buffer => {
         const db = new SQL.Database(new Uint8Array(buffer))
-        const stmt = db.prepare(query)
-
-        const table = document.createElement('table')
-        table.style.borderCollapse = 'collapse'
-        table.style.width = '100%'
-
-        const thead = document.createElement('thead')
-        const tbody = document.createElement('tbody')
-
-        let headers = null
-        while (stmt.step()) {
-          const row = stmt.getAsObject()
-
-          if (!headers) {
-            headers = Object.keys(row)
-            const headerRow = document.createElement('tr')
+        try {
+          const stmt = db.prepare(query)
+          const table = document.createElement('table')
+          table.style.borderCollapse = 'collapse'
+          table.style.width = '100%'
+          const thead = document.createElement('thead')
+          const tbody = document.createElement('tbody')
+          let headers = null
+          while (stmt.step()) {
+            const row = stmt.getAsObject()
+            if (!headers) {
+              headers = Object.keys(row)
+              const headerRow = document.createElement('tr')
+              headers.forEach(header => {
+                const th = document.createElement('th')
+                th.textContent = header
+                th.style.border = '1px solid'
+                th.style.padding = '8px'
+                headerRow.appendChild(th)
+              })
+              thead.appendChild(headerRow)
+            }
+            const dataRow = document.createElement('tr')
             headers.forEach(header => {
-              const th = document.createElement('th')
-              th.textContent = header
-              th.style.border = '1px solid '
-              th.style.padding = '8px'
-              headerRow.appendChild(th)
+              const td = document.createElement('td')
+              td.textContent = row[header]
+              td.style.border = '1px solid'
+              td.style.padding = '8px'
+              dataRow.appendChild(td)
             })
-            thead.appendChild(headerRow)
+            tbody.appendChild(dataRow)
           }
-
-          const dataRow = document.createElement('tr')
-          headers.forEach(header => {
-            const td = document.createElement('td')
-            td.textContent = row[header]
-            td.style.border = '1px solid'
-            td.style.padding = '8px'
-            dataRow.appendChild(td)
-          })
-          tbody.appendChild(dataRow)
+          table.appendChild(thead)
+          table.appendChild(tbody)
+          queryWrapper.appendChild(table)
+        } catch (error) {
+          const errorMessage = 'ERROR: ' + error.message
+          displayError(queryWrapper, errorMessage)
         }
-
-        table.appendChild(thead)
-        table.appendChild(tbody)
-
-        const resultsElement = document.createElement('div')
-        resultsElement.appendChild(table)
-
-        const queryElement = displayText.querySelectorAll('p')[index]
-        queryElement.insertAdjacentElement('afterend', resultsElement)
-
-        scrollToBottom()
         db.close()
+        scrollToBottom()
       })
-
       .catch(error => {
-        console.error('Error fetching the SQLite file:', error)
+        console.error('ERROR:', error)
       })
   })
+}
+
+function displayError (queryWrapper, message) {
+  const errorElement = document.createElement('p')
+  errorElement.textContent = message
+  errorElement.style.color = 'red'
+  queryWrapper.appendChild(errorElement)
+  scrollToBottom()
 }
 
 function scrollToBottom () {
