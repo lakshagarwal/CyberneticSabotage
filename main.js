c6dd20c7bc2351dbd78a156e484730adb5d54e76
Ujjwal Maken <umaken@sfu.ca>
2023-07-10 23:34:03 -0700

    Added new functions for trininity story setup

--- main.js
+++ main.js
@@ -4,11 +4,40 @@ const textarea = document.querySelector('#query-textarea')
 const displayText = document.querySelector('.display-text')
 const form = document.querySelector('#query-form')
 const restartButton = document.getElementById('restart-button')
+const storyline = document.getElementById('trinity_text')
 
 let queryHistory = []
+let currentQueryIndex = 0
 let startTime = null
 let score = 0
+let flag = false
+const queries = [
+  ' Hey Neo! The first task is to list all incidents from the \'Incident\' table.',
+  ' to find the most recent incident involving all models.',
+  ' to find out how many incidents exist in the company for the robot models MegaMech and TurboBot .',
+  ' to find how many exist'
+  // Add more queries here
+]
+storyline.textContent = queries[0]
+const answerKeys = [
+//   // Define answer keys for each query in the same order as the queries array
+//   // Each answer key should be an array of arrays representing the rows and columns of the expected table
+//   // Example: [['column1', 'column2'], ['value1', 'value2']]
+  [
+    // ['incidentID', 'desc', 'timeStamp', 'reportedBy', 'robotID'],
+    [1, 'Robot malfunctioned during production', '2022-02-20 09:30:00', 'Jane Smith', 2],
+    [2, 'Collision with another robot', '2022-03-15 13:45:00', 'Emily Davis', 4]
+  ],
+  //   // Add more answer keys for each query
+  [
+    [2, 'Collision with another robot', '2022-03-15 13:45:00', 'Emily Davis', 4]
+  ],
 
+  [
+    ['MegaMech', 1],
+    ['TurboBot', 1]
+  ]
+]
 let db
 
 function restartGame () {
@@ -19,6 +48,8 @@ function restartGame () {
   updateTimer()
   updateScore()
   initializeDB()
+  storyline.textContent = queries[0]
+  currentQueryIndex = 0
 }
 
 function startGame () {
@@ -27,6 +58,16 @@ function startGame () {
   initializeDB()
 }
 
+function getStory () {
+  const nextQueryIndex = currentQueryIndex + 1
+  if (flag === true && nextQueryIndex < queries.length) {
+    const nextQuery = queries[nextQueryIndex]
+    storyline.textContent = 'Correct! Now the next problem is: ' + nextQuery
+    currentQueryIndex = nextQueryIndex
+  } else {
+    storyline.textContent = 'Oops! Please try again'
+  }
+}
 function updateTimer () {
   const now = Date.now()
   const timeElapsed = Math.round((now - startTime) / 1000)
@@ -57,7 +98,7 @@ form.addEventListener('submit', function (event) {
   scrollToBottom()
 
   executeQuery(query, queryHistory.length - 1, queryWrapper)
-
+  getStory()
   displayText.appendChild(queryWrapper)
   scrollToBottom()
 })
@@ -95,6 +136,7 @@ function displayResults (queryWrapper, result) {
   table.appendChild(thead)
   table.appendChild(tbody)
   queryWrapper.appendChild(table)
+  // flag = true
 }
 
 function displayMessage (queryWrapper, message) {
@@ -120,12 +162,19 @@ function executeQuery (query, index, queryWrapper) {
     const results = db.exec(query)
     if (results.length === 0) {
       displayMessage(queryWrapper, 'Command executed successfully.')
+      flag = false
     } else {
       displayResults(queryWrapper, results[0])
+      if (validateResult(results[0].values, currentQueryIndex)) {
+        flag = true
+      } else {
+        flag = false
+      }
     }
   } catch (error) {
     const errorMessage = 'ERROR: ' + error.message
     displayError(queryWrapper, errorMessage)
+    flag = false
   }
   scrollToBottom()
 }
@@ -137,7 +186,44 @@ function displayError (queryWrapper, message) {
   queryWrapper.appendChild(errorElement)
   scrollToBottom()
 }
-
+function validateResult (resultValues, queryIndex) {
+  const answerKey = answerKeys[queryIndex]
+  console.log(resultValues.length)
+  console.log(answerKey.length)
+  console.log(resultValues[0][0])
+  console.log(answerKey[0][0])
+  console.log(resultValues[0][1])
+  console.log(answerKey[0][1])
+  console.log(resultValues[1])
+  console.log(answerKey[1])
+  // console.log(resultValues[1][1])
+  // console.log(answerKey[1][1])
+  if (!answerKey) {
+    return false // No answer key found for the query
+  }
+  console.log('2nd one')
+  // Check if the number of rows and columns match
+  if (resultValues.length !== answerKey.length) {
+    return false
+  }
+  console.log('3rd one')
+  if (resultValues.length > 0 && resultValues[0].length !== answerKey[0].length) {
+    return false
+  }
+  console.log('4th one')
+  // Check each cell value against the answer key
+  for (let i = 0; i < resultValues.length; i++) {
+    for (let j = 0; j < resultValues[i].length; j++) {
+      console.log('5th one')
+      if (resultValues[i][j] !== answerKey[i][j]) {
+        return false // Cell value does not match the answer key
+      }
+      console.log('6th one')
+    }
+  }
+  console.log('7th  one')
+  return true // All cell values match the answer key
+}
 function scrollToBottom () {
   const displayText = document.querySelector('.display-text')
   displayText.scrollTop = displayText.scrollHeight
