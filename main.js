34e0a5c7b48fe5e9c9051a0ef42dc908bbb2eb24
Karim Khoja <karimkhoja13@yahoo.com>
2023-07-29 19:29:35 -0700

    Implement hints pop-up box with hints working for all the questions (#44)

--- main.js
+++ main.js
@@ -5,7 +5,10 @@ const form = document.querySelector('#query-form')
 const restartButton = document.getElementById('restart-button')
 const storyline = document.getElementById('trinity-text')
 const hintButton = document.getElementById('hint-button')
-const hintContainer = document.getElementById('hint-text')
+const yesButton = document.getElementById('yes')
+const noButton = document.getElementById('no')
+const okayButton = document.getElementById('okay')
+const hintContainer = document.getElementById('modal-content')
 const progressBar = document.getElementById('progress-bar')
 const progressText = document.getElementById('progress-text')
 
@@ -15,7 +18,8 @@ let startTime = null
 let score = 0
 let progress = 10
 let flag = false
-let hintCounter = 1
+let hintCounter = 0
+let subArrayLength
 
 const queries = [
   ' Hey Detective! The first task is to list all incidents from the \'Incident\' table.',
@@ -35,21 +39,23 @@ const queries = [
 storyline.textContent = queries[0]
 
 const hints = [
-  [['Hint : For your first hint for the 1st problem, it\'s going to cost you 15 points. Click on the Hint button to use it'], ['Hint 1: You can use the Select statement here.'], ['Hint 2: The structure of the query will look like SELECT _ FROM [TableName].']],
-  [['Hint : For your first hint for the 2nd problem, it\'s going to cost you 15 points. Click on the Hint button to use it'], ['Hint 1: You might want to look at the timestamp to find the most recent incident.'], ['Hint 2: Consider using the keyword LIMIT to get the most recent incident.']],
-  [['Hint : For your first hint for the 3rd problem, it\'s going to cost you 15 points. Click on the Hint button to use it'], ['Hint 1: Use a `JOIN` operation to join Robot and Incident Table.'], ['Hint 2: Use `GROUP BY` and `COUNT` operations to count the number of incidents.']],
-  [['Hint : For your first hint for the 4th problem, it\'s going to cost you 15 points. Click on the Hint button to use it'], ['Hint 1: Use the lastUpdateOn column and specify dates to check the interval.'], ['Use `DISTINCT`operation to get unique robotIDs and then count accordingly.']],
-  [['Hint : For your first hint for the 5th problem, it\'s going to cost you 15 points. Click on the Hint button to use it'], ['Hint 1: Use nested subqueries here.'], ['Hint 2: In the inner query get the employeeID of the employees who have updated those models from the Robot table.']],
-  [['Hint : For your first hint for the 6th problem, it\'s going to cost you 15 points. Click on the Hint button to use it'], ['Hint 1: Use`UPDATE`and `SET`operations here.'], ['Hint 2: Do not forget to display the Robot table by using the same structure as in Query 1']],
-  [['Hint : For your first hint for the 7th problem, it\'s going to cost you 15 points. Click on the Hint button to use it'], ['Hint 1: You need to use`GROUP BY` and `ORDER BY` operations here '], ['Hint 2: Do not forget to put the LIMIT as 1 to get only the one employee with the highest number of incidents']],
-  [['Hint : For your first hint for the 8th problem, it\'s going to cost you 15 points. Click on the Hint button to use it'], ['Hint 1 for question 8'], ['Hint 2 for question 8']],
-  [['Hint : For your first hint for the 9th problem, it\'s going to cost you 15 points. Click on the Hint button to use it'], ['Hint 1 for question 9'], ['Hint 2 for question 9']],
-  [['Hint : For your first hint for the 10th problem, it\'s going to cost you 15 points. Click on the Hint button to use it'], ['Hint 1 for question 10'], ['Hint 2 for question 10']],
-  [['Hint : For your first hint for the 11th problem, it\'s going to cost you 15 points. Click on the Hint button to use it'], ['Hint 1 for question 11'], ['Hint 2 for question 11']],
-  [['Hint : For your first hint for the 12th problem, it\'s going to cost you 15 points. Click on the Hint button to use it'], ['Hint 1 for question 12'], ['Hint 2 for question 12']],
-  [['Hint : For your first hint for the 12th problem, it\'s going to cost you 15 points. Click on the Hint button to use it'], ['Hint 1 for question 13'], ['Hint 2 for question 13']]
+  [['Hint 1: You can use the Select statement here.'], ['Hint 2: The structure of the query will look like SELECT _ FROM [TableName].']],
+  [['Hint 1: You might want to look at the timestamp to find the most recent incident.'], ['Hint 2: Consider using the keyword LIMIT to get the most recent incident.']],
+  [['Hint 1: Use a `JOIN` operation to join Robot and Incident Table.'], ['Hint 2: Use `GROUP BY` and `COUNT` operations to count the number of incidents.']],
+  [['Hint 1: Use the lastUpdateOn column and specify dates to check the interval.'], ['Use `DISTINCT`operation to get unique robotIDs and then count accordingly.']],
+  [['Hint 1: Use nested subqueries here.'], ['Hint 2: In the inner query get the employeeID of the employees who have updated those models from the Robot table.']],
+  [['Hint 1: Use`UPDATE`and `SET`operations here.'], ['Hint 2: Do not forget to display the Robot table by using the same structure as in Query 1']],
+  [['Hint 1: You need to use`GROUP BY` and `ORDER BY` operations here '], ['Hint 2: Do not forget to put the LIMIT as 1 to get only the one employee with the highest number of incidents']],
+  [['Hint 1 for question 8'], ['Hint 2 for question 8']],
+  [['Hint 1 for question 9'], ['Hint 2 for question 9']],
+  [['Hint 1 for question 10'], ['Hint 2 for question 10']],
+  [['Hint 1 for question 11'], ['Hint 2 for question 11']],
+  [['Hint 1 for question 12'], ['Hint 2 for question 12']],
+  [['Hint 1 for question 13'], ['Hint 2 for question 13']]
 ]
+
 hintContainer.textContent = hints[0][0]
+
 const answerKeys = [
   [
     [1, 'Robot malfunctioned during production', '2022-02-20 09:30:00', 'Jane Smith', 2],
@@ -134,7 +140,7 @@ function restartGame () {
   initializeDB()
   storyline.textContent = queries[0]
   currentQueryIndex = 0
-  hintCounter = 1
+  hintCounter = 0
   hintContainer.textContent = hints[0][0]
 }
 
@@ -152,9 +158,8 @@ function getStory () {
   if (flag === true && nextQueryIndex < queries.length) {
     const nextQuery = queries[nextQueryIndex]
     storyline.textContent = 'Excellent! Next, ' + nextQuery
+    hintCounter = 0
     currentQueryIndex = nextQueryIndex
-    hintCounter = 1
-    hintContainer.textContent = hints[currentQueryIndex][0]
     updateScore(10)
     updateProgressBar(8)
   } else {
@@ -205,18 +210,70 @@ form.addEventListener('submit', function (event) {
   scrollToBottom()
 })
 
-hintButton.addEventListener('click', getHint)
+const hintsCost = [30, 40, 50]
+
+yesButton.addEventListener('click', getHint)
+
 function getHint () {
   const hintIndex = currentQueryIndex
   const hintArray = hints[hintIndex]
-  const subArrayLength = hintArray.length
+  subArrayLength = hintArray.length
+  console.log('Sub array length = ' + subArrayLength)
+  subArrayLength = hintArray.length
+  console.log('Sub array length = ' + subArrayLength)
 
   if (hintCounter < subArrayLength) {
     const hint = hintArray[hintCounter]
-    hintContainer.textContent = hint
+    storyline.textContent = hint
     hintCounter = hintCounter + 1
   } else {
-    hintContainer.textContent = 'No more hints available.'
+    storyline.textContent = 'No more hints available.'
+  }
+}
+
+const modal = document.getElementById('hint-modal')
+
+const spanClose = document.getElementsByClassName('close')[0]
+
+hintButton.onclick = function () {
+  modal.style.display = 'block'
+  yesButton.style.display = 'inline'
+  noButton.style.display = 'inline'
+  okayButton.style.display = 'none'
+  console.log('hint counter: ' + hintCounter + ' sub array length = ' + subArrayLength)
+  if (hintCounter !== subArrayLength) {
+    hintContainer.textContent = 'Hint : For hint # ' +
+  (hintCounter + 1) +
+  ' for this problem, it\'s going to cost you ' +
+  hintsCost[hintCounter] +
+  '. Click on the Hint button to use it'
+  } else {
+    yesButton.style.display = 'none'
+    noButton.style.display = 'none'
+    okayButton.style.display = 'inline'
+    hintContainer.textContent = 'Sorry! No more hints are availabe for this question.'
+  }
+}
+
+yesButton.onclick = function () {
+  modal.style.display = 'none'
+}
+
+noButton.onclick = function () {
+  modal.style.display = 'none'
+}
+
+spanClose.onclick = function () {
+  modal.style.display = 'none'
+}
+
+okayButton.onclick = function () {
+  modal.style.display = 'none'
+}
+
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = 'none'
   }
 }
 
